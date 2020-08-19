168cf67fb5663274d37533c99d5a4f14adb4230e
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-08-20 02:07:45 +0300

    add handlesubmit to retrieve state values

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,7 +1,16 @@
 import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import categories from '../variables/Categories';
+import createBook from '../actions/books';
 
-const BookForm = ({ book }) => {
+const mapDispatchProps = dispatch => ({
+  addBook: book => {
+    dispatch(createBook(book));
+  },
+});
+
+const BookForm = ({ newBook }) => {
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('');
 
@@ -10,18 +19,25 @@ const BookForm = ({ book }) => {
 
   const handleSubmit = e => {
     e.preventDefault();
+    const book = { title, category };
+    newBook(book);
+    setTitle('');
+    setCategory('');
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value="" name="title" onChange={handleTextChange} />
+      <input type="text" value="" name="title" placeholder="Please add a new book" onChange={handleTextChange} />
       <select value="" name="category" required onChange={handleOptionChange}>
         { categories.map(category => <option key={category} value={category}>{category}</option>) }
       </select>
       <button type="submit">Submit</button>
     </form>
-  )
-
+  );
 };
 
-export default BookForm;
+BookForm.propTypes = {
+  newBook: PropTypes.func.isRequired,
+};
+
+export default connect(null, mapDispatchProps)(BookForm);
