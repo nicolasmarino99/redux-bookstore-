618e1911b771e402b1a87cc25ee64b8afaf95ddc
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-08-19 20:17:21 +0300

    add useState for saving title and category state

--- src/containers/BooksForm.js
+++ src/containers/BooksForm.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import categories from '../variables/Categories';
 
-const BookForm = () => (
-  <form>
-    <input type="text" value="" name="title" />
-    <select value="" name="category">
-      { categories.map(category => <option key={category} value={category}>{category}</option>) }
-    </select>
-    <button type="submit">Submit</button>
-  </form>
+const BookForm = ({ book }) => {
+  const [title, setTitle] = useState('');
+  const [category, setCategory] = useState('');
 
-);
+  return (
+    <form>
+      <input type="text" value="" name="title" />
+      <select value="" name="category">
+        { categories.map(category => <option key={category} value={category}>{category}</option>) }
+      </select>
+      <button type="submit">Submit</button>
+    </form>
+  )
+
+};
 
 export default BookForm;
