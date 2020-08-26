b786ba9dc885d43af6e0ca829da361c47fd1c6ab
Nicolas Mariño <nicolasmarinoparra@gmail.com>
2020-08-26 20:50:23 +0300

    add category filter component

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import categories from '../variables/Categories';
+
+const CategoryFilter = ({ handleOptionChange }) => (
+  <form>
+    <label htmlFor="category-filter">
+      <select name="category-filter" id="category-filter" required onChange={handleOptionChange}>
+        { categories.map(category => <option key={category} value={category}>{category}</option>) }
+      </select>
+    </label>
+  </form>
+);
+
+CategoryFilter.propTypes = {
+  handleOptionChange: PropTypes.func.isRequired,
+};
